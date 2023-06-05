df09926e882cae18bd418127eafe1c121d2aa359
Grace Nguyen <nng.grace@gmail.com>
2023-06-05 13:54:08 -0400

    grace: wip to add gradient to welcomeAcitivity

--- app/src/main/java/com/example/jukebox/WelcomeActivity.kt
+++ app/src/main/java/com/example/jukebox/WelcomeActivity.kt
@@ -6,28 +6,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.style.TextDirection.Companion.Content
+import com.example.jukebox.ui.theme.Black
+import com.example.jukebox.ui.theme.PurpleNeon
 
 class WelcomeActivity  : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                Test("test text")
-            }
+            Test("test text")
         }
     }
 }
 
 @Composable
 fun Test(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        PurpleNeon,
+                        Black
+                    )
+                )
+            )
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
