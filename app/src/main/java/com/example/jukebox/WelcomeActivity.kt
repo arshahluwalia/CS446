f0b613300a2fb0d289239c317b325b7d08777a05
Grace Nguyen <nng.grace@gmail.com>
2023-06-05 14:39:22 -0400

    grace: welcome page completed (missing 1 button)

--- app/src/main/java/com/example/jukebox/WelcomeActivity.kt
+++ app/src/main/java/com/example/jukebox/WelcomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jukebox
 
 import android.os.Bundle
+import android.util.Size
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,37 +12,88 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDirection.Companion.Content
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jukebox.ui.theme.Black
 import com.example.jukebox.ui.theme.PurpleNeon
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.scale
 
 class WelcomeActivity  : ComponentActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Test("test text")
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Background()
+                }
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Test(name: String, modifier: Modifier = Modifier) {
+fun Background() {
+    var text by remember { mutableStateOf("") }
     Box(
         modifier = Modifier
+            .fillMaxSize()
+//            .scale(maxOf(aspectRatio, 1f), maxOf(1 / aspectRatio, 1f))
             .background(
-                brush = Brush.verticalGradient(
+                brush = Brush.radialGradient(
                     colors = listOf(
                         PurpleNeon,
                         Black
-                    )
+                    ),
                 )
             )
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+            Text(
+                text = "Jukebox",
+                fontSize = 60.sp,
+                color = Color.White
+            )
+            Text(
+                text = "Stop looking for the party aux",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+        }
+        TextField(
+            value = text,
+            onValueChange = {
+                val it = ""
+                text = it
+            },
+            label = { Text("Enter your room code") },
+            maxLines = 2,
+//            textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .padding(20.dp)
+                .align(Alignment.BottomCenter)
         )
     }
-
 }
--- app/src/main/java/com/example/jukebox/ui/theme/Color.kt
+++ app/src/main/java/com/example/jukebox/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package com.example.jukebox.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PurpleNeon = Color(0xBC13FE)
-val Black = Color(0x000000)
+val PurpleNeon = Color(0xFFBC13FE)
+val Black = Color(0xFF000000)
 val Pink80 = Color(0xFFEFB8C8)
