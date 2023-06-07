32f6b8577fac73d4baa4f545f65dfa5e00a864eb
Grace Nguyen <nng.grace@gmail.com>
2023-06-07 15:23:54 -0400

    grace - complete reusableBackground

--- app/src/main/java/com/example/jukebox/ReusableBackground.kt
+++ app/src/main/java/com/example/jukebox/ReusableBackground.kt
@@ -13,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,17 +24,16 @@ import com.example.jukebox.ui.theme.JukeboxTheme
 import com.example.jukebox.ui.theme.Black
 import com.example.jukebox.ui.theme.PurpleNeon
 
-class ReusableBackground : ComponentActivity() {
-
-}
 fun Modifier.reusableBackground() = composed {
     val configuration = LocalConfiguration.current
-//        val screenHeight = configuration.screenHeightDp.dp
-//        val screenWidth = configuration.screenWidthDp.dp
+    val boxHeight = with(LocalDensity.current) { configuration.screenHeightDp.dp.toPx() }
+    val boxWidth = with(LocalDensity.current) { configuration.screenWidthDp.dp.toPx() }
+    val aspectRatio = boxWidth/boxHeight
     this.fillMaxSize()
-        .scale(0.0F, 2.0F)
+        .scale(maxOf(aspectRatio, 1f), maxOf(3 / aspectRatio, 1f))
         .background(
             brush = Brush.radialGradient(
+                center = Offset(x = 0F, y = 2*boxHeight / 3),
                 colors = listOf(
                     PurpleNeon,
                     Black
--- app/src/main/java/com/example/jukebox/SongQueueActivity.kt
+++ app/src/main/java/com/example/jukebox/SongQueueActivity.kt
@@ -6,21 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class SongQueueActivity  : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
+            Column(
                 modifier = Modifier.reusableBackground(),
             ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-
-                }
+                Text(text = "This is the song queue", color = Color.White)
             }
         }
+
     }
 }
