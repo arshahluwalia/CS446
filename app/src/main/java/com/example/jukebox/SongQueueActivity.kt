d707f783a236490f4040431e2b61912ff62aa310
Kenneth Yang <k233yang@uwaterloo.ca>
2023-06-07 13:40:52 -0400

    kenny: add song queue activity

---
+++ app/src/main/java/com/example/jukebox/SongQueueActivity.kt
@@ -0,0 +1,14 @@
+package com.example.jukebox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class SongQueueActivity  : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
