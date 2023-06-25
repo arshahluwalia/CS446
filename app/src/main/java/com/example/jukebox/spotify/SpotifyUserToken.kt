384d477526ceef686f8c60ff5f3f78481ec82adc
Grace Nguyen <nng.grace@gmail.com>
2023-06-25 18:56:56 -0400

    grace - cont investigating my search api give null response

--- app/src/main/java/com/example/jukebox/spotify/SpotifyUserToken.kt
+++ app/src/main/java/com/example/jukebox/spotify/SpotifyUserToken.kt
@@ -1,7 +1,7 @@
 package com.example.jukebox.spotify
 
 object SpotifyUserToken {
-
+	// TODO: Deprecate this and pull from the database so we dont have 2 sources of truth
 	private var token = ""
 
 	fun getToken() = token
