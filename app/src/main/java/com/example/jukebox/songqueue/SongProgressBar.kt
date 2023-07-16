b9f7b2792727b109b0368a11d86ec4b069f2c4d6
Grace Nguyen <nng.grace@gmail.com>
2023-07-15 23:37:55 -0400

    grace - resolve timer implementation conflict

--- app/src/main/java/com/example/jukebox/songqueue/SongProgressBar.kt
+++ app/src/main/java/com/example/jukebox/songqueue/SongProgressBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.jukebox.Song
 import com.example.jukebox.spotify.task.SpotifySongControlTask.playSong
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
@@ -40,15 +42,15 @@ fun SongProgressBar(
         .fillMaxWidth()
         .padding(horizontal = 20.dp, vertical = 10.dp)
     ) {
-       LaunchedEffect(Unit) {
-            // Start a coroutine that fetches the API and updates the position every second
-            while (true) {
-                // Pass host token to get position
-                var playBackState = getPlaybackState(hTokens);
-                sliderValue = (playBackState.second as? Int)?.toFloat() ?: 0f
-                delay(1000) // Delay for 1 second before fetching again
-            }
-        }
+//       LaunchedEffect(Unit) {
+//            // Start a coroutine that fetches the API and updates the position every second
+//            while (true) {
+//                // Pass host token to get position
+//                var playBackState = getPlaybackState(hTokens);
+//                sliderValue = (playBackState.second as? Int)?.toFloat() ?: 0f
+//                delay(1000) // Delay for 1 second before fetching again
+//            }
+//        }
         Slider(
             modifier = Modifier
                 .fillMaxWidth(),
